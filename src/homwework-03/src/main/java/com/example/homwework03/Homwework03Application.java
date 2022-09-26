ddd285347604078889182aa6dac8640a3d98f5ba
recepergan <68998200+recepergan@users.noreply.github.com>
2022-09-26 15:55:03 +0300

    Course controller,service,DAO , model added. All courses listed

---
+++ src/homwework-03/src/main/java/com/example/homwework03/Homwework03Application.java
@@ -0,0 +1,13 @@
+package com.example.homwework03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homwework03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homwework03Application.class, args);
+    }
+
+}
---
+++ src/homwework-03/src/test/java/com/example/homwework03/Homwework03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homwework03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homwework03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
